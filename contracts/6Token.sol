e178ec685ae95562b7c28427a8d414e74ce92751
Claudia <claudiabarcelovaldes40@gmail.com>
2023-02-22 22:59:40 +0100

    add lvl 6 solution

--- contracts/6Token.sol
+++ contracts/6Token.sol
@@ -21,30 +21,4 @@ contract Token {
   }
 }
 
-contract Token2 {
-  mapping(address => uint256) balances;
-  uint public totalSupply;
-
-  constructor(uint _initialSupply) public {
-    balances[msg.sender] = totalSupply = _initialSupply;
-  }
-
-  function transfer(address _to, uint _value) public returns (bool) {
-    require(balances[msg.sender] - _value >= 0);
-    balances[msg.sender] -= _value;
-    balances[_to] += _value;
-    return true;
-  }
-
-  function increment(uint256 _value) public view returns (uint) {
-    return balances[msg.sender] - _value;
-  }
-
-  function isIt(uint256 _value) public pure returns (bool) {
-    return 20 - _value >= 0;
-  }
-
-  function balanceOf(address _owner) public view returns (uint balance) {
-    return balances[_owner];
-  }
-}
+// Solution: on the console, contract.transfer(toSomeone, 21)
